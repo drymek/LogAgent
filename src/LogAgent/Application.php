f289113565db875b3ce65b708462c924a52def6d
Marcin Dryka <marcin@dryka.pl>
2014-01-30 13:59:01 +0100

    DI and config for commands

--- src/LogAgent/Application.php
+++ src/LogAgent/Application.php
@@ -3,6 +3,9 @@
 namespace LogAgent;
 
 use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
 
 class Application extends BaseApplication
 {
@@ -12,5 +15,21 @@ class Application extends BaseApplication
     public function __construct()
     {
         parent::__construct(static::NAME, static::VERSION);
+
+        $container = new ContainerBuilder();
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
+        $loader->load('commands.yml');
+
+        $this->initializeCommands($container);
+    }
+
+    private function initializeCommands(ContainerBuilder $container)
+    {
+        $commands = $container->findTaggedServiceIds('logagent.command');
+
+        foreach ($commands as $id => $attributes) {
+            $command = $container->get($id);
+            $this->add($command);
+        }
     }
 }
