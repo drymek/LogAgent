b840bec1c499d181257418ca66ef2c44aa50a167
Marcin Dryka <marcin@dryka.pl>
2014-02-06 12:47:10 +0100

    Disk space collector content

--- src/LogAgent/Collector/DiskSpace.php
+++ src/LogAgent/Collector/DiskSpace.php
@@ -2,8 +2,19 @@
 
 namespace LogAgent\Collector;
 
+use Symfony\Component\Process\ProcessBuilder;
+
 class DiskSpace implements CollectorInterface
 {
+    private $builder;
+    private $process;
+    private $devices;
+
+    public function __construct(ProcessBuilder $builder)
+    {
+        $this->builder = $builder;
+    }
+
     public function getAlias()
     {
         return 'disk_space';
@@ -11,11 +22,41 @@ class DiskSpace implements CollectorInterface
 
     public function configure(array $config)
     {
-        var_dump($config);
+        $this->process = $this->builder->setPrefix('df')->getProcess();
+        $this->devices = $config['devices'];
     }
 
     public function execute()
     {
-        return 'xxx';
+        $this->process->run();
+
+        if (!$this->process->isSuccessful()) {
+            throw new \RuntimeException($this->process->getErrorOutput());
+        }
+
+        $output = $this->process->getOutput();
+
+        return $this->parseOutput($output);
+    }
+
+    private function parseOutput($output)
+    {
+        $lines = explode("\n", $output);
+        $result = '';
+
+        foreach ($lines as $line) {
+            if (empty($line)) {
+                continue;
+            }
+
+            list ($device, $block, $used, $available, $percentage, $mount) = 
+                preg_split('/\s+/', $line);
+
+            if (in_array($device, $this->devices)) {
+                $result .= implode('|', array($device, $used, $available, $mount)) . "\n";
+            }
+        }
+
+        return $result;
     }
 }
